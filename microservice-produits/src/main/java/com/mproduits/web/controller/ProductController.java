efd7e0b1f808f6b93f863a68d8574a72c4b1813d
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-06-10 15:44:55 +0200

    create docker-compose.yaml fo Zipkin Server

--- microservice-produits/src/main/java/com/mproduits/web/controller/ProductController.java
+++ microservice-produits/src/main/java/com/mproduits/web/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.mproduits.web.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.mproduits.web.exceptions.ProductNotFoundException;
 
 @RestController
 public class ProductController {
+	org.slf4j.Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     ProductDao productDao;
@@ -31,6 +33,8 @@ public class ProductController {
         if(products.isEmpty()) throw new ProductNotFoundException("Aucun produit n'est disponible à la vente");
         
         List<Product> listeLimitee = products.subList(0, appProperties.getLimitDeProduits());
+
+        log.info("Récupération de la liste des produits");
         
         return  listeLimitee ;
 
