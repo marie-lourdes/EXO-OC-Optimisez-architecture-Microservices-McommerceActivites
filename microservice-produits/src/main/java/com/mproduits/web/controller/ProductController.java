6050afdb18b31127990a4578ef912d73071858a4
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-06-15 14:09:41 +0200

    microservice-produits: implements HealthIndicator and method health() in ProductController.java to customize indicator actutaor health to DOWN if not product in BDD

--- microservice-produits/src/main/java/com/mproduits/web/controller/ProductController.java
+++ microservice-produits/src/main/java/com/mproduits/web/controller/ProductController.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,40 +17,57 @@ import com.mproduits.model.Product;
 import com.mproduits.web.exceptions.ProductNotFoundException;
 
 @RestController
-public class ProductController {
+public class ProductController implements HealthIndicator {
 	org.slf4j.Logger log = LoggerFactory.getLogger(this.getClass());
 
-    @Autowired
-    ProductDao productDao;
-    
-    @Autowired
-    ApplicationPropertiesConfig appProperties; // properties personnalisé :mes-configs.limitDeProduits= 4
+	@Autowired
+	ProductDao productDao;
 
-    // Affiche la liste de tous les produits disponibles
-    @GetMapping(value = "/Produits")
-    public List<Product> listeDesProduits(){
+	@Autowired
+	ApplicationPropertiesConfig appProperties; // properties personnalisé :mes-configs.limitDeProduits= 4
 
-        List<Product> products = productDao.findAll();
+	/*
+	 * surcharger method health de l interfae HealthIndicator de Spring Actuator et
+	 * personnaliser l indicateur health à down si il n y a pas de produits ds la
+	 * BDD ou up si il y en a
+	 */
+	@Override
+	public Health health() {
+		List<Product> products = productDao.findAll();
 
-        if(products.isEmpty()) throw new ProductNotFoundException("Aucun produit n'est disponible à la vente");
-        
-        List<Product> listeLimitee = products.subList(0, appProperties.getLimitDeProduits());
+		if (products.isEmpty()) {
+			return Health.down().build();
+		}
 
-        log.info("Récupération de la liste des produits");
-        
-        return  listeLimitee ;
+		return Health.down().build();
+	}
 
-    }
+	// Affiche la liste de tous les produits disponibles
+	@GetMapping(value = "/Produits")
+	public List<Product> listeDesProduits() {
 
-    //Récuperer un produit par son id
-    @GetMapping( value = "/Produits/{id}")
-    public Optional<Product> recupererUnProduit(@PathVariable int id) {
+		List<Product> products = productDao.findAll();
 
-        Optional<Product> product = productDao.findById(id);
+		if (products.isEmpty())
+			throw new ProductNotFoundException("Aucun produit n'est disponible à la vente");
 
-        if(!product.isPresent())  throw new ProductNotFoundException("Le produit correspondant à l'id " + id + " n'existe pas");
+		List<Product> listeLimitee = products.subList(0, appProperties.getLimitDeProduits());
 
-        return product;
-    }
+		log.info("Récupération de la liste des produits");
+
+		return listeLimitee;
+
+	}
+
+	// Récuperer un produit par son id
+	@GetMapping(value = "/Produits/{id}")
+	public Optional<Product> recupererUnProduit(@PathVariable int id) {
+
+		Optional<Product> product = productDao.findById(id);
+
+		if (!product.isPresent())
+			throw new ProductNotFoundException("Le produit correspondant à l'id " + id + " n'existe pas");
+
+		return product;
+	}
 }
-
